0cc312c078ceb9ebea940b94d1ba4ee2a85d2c45
Pedro <pjsillescas@gmail.com>
2019-03-12 23:29:00 +0100

    Patrolling guard component.

--- Source/TestingGrounds/BTChooseNextWaypoint.cpp
+++ Source/TestingGrounds/BTChooseNextWaypoint.cpp
@@ -4,15 +4,26 @@
 #include "Runtime/AIModule/Classes/BehaviorTree/BlackboardComponent.h"
 #include "Runtime/AIModule/Classes/AIController.h"
 
-#include "PatrollingGuard.h"
+//#include "PatrollingGuard.h"
+#include "PatrollingGuardComponent.h"
 
 void UBTChooseNextWaypoint::GetPatrolPoints(UBehaviorTreeComponent& OwnerComp)
 {
+	APawn* Pawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (Pawn == nullptr) return;
+	UPatrollingGuardComponent* PatrollingGuardComponent = Cast<UPatrollingGuardComponent>(Pawn->GetComponentByClass(UPatrollingGuardComponent::StaticClass()));
+	
+	if (PatrollingGuardComponent == nullptr) return;
+
+	RoutePoints = PatrollingGuardComponent->GetPatrolPoints();
+
+	/*
 	APatrollingGuard* Guard = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
 
 	if (Guard == nullptr) return;
 
 	RoutePoints = Guard->GetPatrolPoints();
+	*/
 }
 
 EBTNodeResult::Type UBTChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
@@ -23,8 +34,15 @@ EBTNodeResult::Type UBTChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& O
 	UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
 	int32 Index = Blackboard->GetValueAsInt(NextWaypointIndex.SelectedKeyName);
 
-	Blackboard->SetValueAsObject(Waypoint.SelectedKeyName, RoutePoints[Index]);
-	Blackboard->SetValueAsInt(NextWaypointIndex.SelectedKeyName, (Index + 1) % RoutePoints.Num());
-	
+	if (RoutePoints.Num() == 0)
+	{
+		UE_LOG(LogTemp,Warning,TEXT("Guard %s is missing patrol points"),*(OwnerComp.GetAIOwner()->GetPawn()->GetName()));
+	}
+	else
+	{
+		Blackboard->SetValueAsObject(Waypoint.SelectedKeyName, RoutePoints[Index]);
+		Blackboard->SetValueAsInt(NextWaypointIndex.SelectedKeyName, (Index + 1) % RoutePoints.Num());
+	}
+
 	return EBTNodeResult::Succeeded;
 }
